74c1ead7ff64bd720363b8e2edb2a9230ee2a195
kwonja <kownja87@gmail.com>
2023-06-02 16:18:46 +0900

    update

--- Yelp/app.py
+++ Yelp/app.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template
-
+import pymongo
+from urllib import parse
 app = Flask(__name__)
 
+host = "localhost"
+port = "27017"
+user = "root"
+pwd = "root"
+db = "yelp"
+client = pymongo.MongoClient("mongodb://{}:".format(user)+ parse.quote(pwd)+ "@{}:{}/{}".format(host,port,db))
+db_conn = client.get_database(db)
+
 @app.route("/")
-def hello_world():
+def main():
+    
     return render_template('index.html')
+
+@app.route("/business")
+def business():
+    return render_template('business.html')
+@app.route("/user")
+def user():
+    collection_user = db_conn.get_collection("yelp_user")
+    query = {}
+    results = collection_user.find(query).sort('review_count',-1).limit(10)
+
+    return render_template('user.html',data=results)
