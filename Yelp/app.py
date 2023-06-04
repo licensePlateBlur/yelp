af32ce09f2a0908a419a53bb0c9d414245864aee
youngjaeee <youngjaeee@chungbuk.ac.kr>
2023-06-04 16:42:02 +0900

    added showing image feature by searching business_name

--- Yelp/app.py
+++ Yelp/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, url_for
 import pymongo
 from urllib import parse
+import urllib.parse
 app = Flask(__name__)
 
 host = "localhost"
@@ -16,9 +17,53 @@ def main():
     
     return render_template('index.html')
 
+
+@app.route('/image/<filename>')
+def display_image(filename):
+    print("image return requested")
+    return send_from_directory('C:/Users/yjson/Downloads/yelpPhoto/photos', filename)
+
 @app.route("/business")
 def business():
     return render_template('business.html')
+
+
+
+@app.route('/business/<name>')
+def search_business(name):
+
+    decoded_name = urllib.parse.unquote(name)
+    collection_business = db_conn.get_collection("yelp_business")
+    query = {'name': decoded_name}
+
+    # Search for the business in the "yelp_business" collection
+    business = collection_business.find_one(query)
+
+    if business:
+        # Get the business ID
+        business_id = business['business_id']
+
+        # Search for the corresponding image in the "yelp_photo" collection
+        collection_photo = db_conn.get_collection("yelp_photo")
+        query_photo = {'business_id': business_id}
+
+        photo = collection_photo.find_one(query_photo)
+
+        if photo:
+            # Get the photo ID (image filename without extension)
+            photo_id = photo['photo_id']
+            print("photo_id below")
+            print(photo_id)
+
+            # Render the template and pass the image filename
+            return render_template('business.html', image_filename=url_for('display_image', filename=photo_id + '.jpg'))
+
+    # Business not found or image not found
+    return 'Business not found or image not available.'
+
+
+
+
 @app.route("/user")
 def user():
     collection_user = db_conn.get_collection("yelp_user")
