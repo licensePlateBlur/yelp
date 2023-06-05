bb89ac22244f9112353f2a7efac2c4a261e99e7c
kwonja <kownja87@gmail.com>
2023-06-05 16:56:27 +0900

    update

--- Yelp/app.py
+++ Yelp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, url_for, request,send_from_directory,make_response
+from flask import Flask, render_template, send_from_directory, url_for, request,send_from_directory,make_response,jsonify,g
 import pymongo
 import pandas as pd
 import json
@@ -15,6 +15,7 @@ pwd = "root"
 db = "yelp"
 client = pymongo.MongoClient("mongodb://{}:".format(user)+ parse.quote(pwd)+ "@{}:{}/{}".format(host,port,db))
 db_conn = client.get_database(db)
+
 
 # 영재 이거 추가하면됨
 @app.route('/static/css/<path:path>')
@@ -103,3 +104,89 @@ def date():
         results = collection_review.aggregate(pipelines)
         return render_template('business.html',data=results)
     return "에러입니다."
+
+@app.route('/place', methods=['GET', 'POST'])
+def place():
+    #평점을 높게준 사람이 (평균 평점 4점 이상) 낮게 평가한(2점 이하) (좋은 장소 추출)
+    collection_review = db_conn.get_collection("yelp_review")
+    pipelines = list()
+    pipelines.append({'$lookup':{'from':"yelp_user",'localField':"user_id", 'foreignField':"user_id", 'as':"user"}})
+    pipelines.append({'$lookup':{'from':"yelp_business",'localField':"business_id", 'foreignField':"business_id", 'as':"business"}})
+    pipelines.append({'$match':{"user.average_stars":{'$lte':2}, 'stars':{'$gt':4}}})
+    pipelines.append({'$sort':{'stars': -1}})
+    pipelines.append({'$project': {'_id': 0,'business_id':1, 'business_name': {'$arrayElemAt': ['$business.name', 0]}}})
+    pipelines.append({'$limit':20})
+    good = collection_review.aggregate(pipelines)
+
+    #평점을 낮게준 사람이 (평균 평점 2점 이하) 높게 평가한(4점 이상) (좋은 장소 추출)
+    collection_review = db_conn.get_collection("yelp_review")
+    pipelines = list()
+    pipelines.append({'$lookup':{'from':"yelp_user",'localField':"user_id", 'foreignField':"user_id", 'as':"user"}})
+    pipelines.append({'$lookup':{'from':"yelp_business",'localField':"business_id", 'foreignField':"business_id", 'as':"business"}})
+    pipelines.append({'$match':{"user.average_stars":{'$gte':4}, 'stars':{'$lte':2}}})
+    pipelines.append({'$sort':{'stars': -1}})
+    pipelines.append({'$project': {'_id': 0,'business_id':1, 'business_name': {'$arrayElemAt': ['$business.name', 0]}, 'text':1}})
+    pipelines.append({'$limit':20})
+    bad = collection_review.aggregate(pipelines)
+
+
+    return render_template('place.html',good=good , bad=bad)
+
+@app.route('/category', methods=['GET', 'POST'])
+def showCate():
+    collection_busi = db_conn.get_collection("yelp_business")
+    pipelines = list()
+    pipelines.append({'$unwind':"$categories"})
+    pipelines.append({'$group': {'_id': "null",'uniqueCategories': {"$addToSet": "$categories"}}})
+    pipelines.append({'$project':{'_id': 0, 'categories': "$uniqueCategories"}})
+    pipelines.append({'$limit':100})
+    global_category = collection_busi.aggregate(pipelines)
+
+    collection_busi = db_conn.get_collection("yelp_business")
+    pipelines = list()
+    pipelines.append({'$group': {'_id': "null",'uniqueCities': {"$addToSet": "$city"}}})
+    pipelines.append({'$project':{'_id': 0, 'city': "$uniqueCities"}})
+    pipelines.append({'$limit':100})
+    global_city= collection_busi.aggregate(pipelines)
+
+
+    return render_template('category.html',category=global_category, city=global_city)
+
+@app.route('/cate', methods=['GET', 'POST'])
+def searchBusi1():
+    collection_busi = db_conn.get_collection("yelp_business")
+    pipelines = list()
+    pipelines.append({'$unwind':"$categories"})
+    pipelines.append({'$group': {'_id': "null",'uniqueCategories': {"$addToSet": "$categories"}}})
+    pipelines.append({'$project':{'_id': 0, 'categories': "$uniqueCategories"}})
+    pipelines.append({'$limit':100})
+    global_category = collection_busi.aggregate(pipelines)
+
+    collection_busi = db_conn.get_collection("yelp_business")
+    pipelines = list()
+    pipelines.append({'$group': {'_id': "null",'uniqueCities': {"$addToSet": "$city"}}})
+    pipelines.append({'$project':{'_id': 0, 'city': "$uniqueCities"}})
+    pipelines.append({'$limit':100})
+    global_city= collection_busi.aggregate(pipelines)
+    cate = request.form['cate']
+    cy = request.form['cy'] 
+    option = request.form['option']
+    print(cate + cy + option)
+
+    collection_busi = db_conn.get_collection("yelp_business")
+    query = {
+        "categories":cate,
+        "city":cy
+    }
+
+    if(option == 'asc'):
+        results = collection_busi.find(query).sort('stars',1)
+        print("오름")
+        return render_template('category.html',data=results,category=global_category, city=global_city) 
+    elif(option == 'desc'):
+        print("내림")
+        results = collection_busi.find(query).sort('stars',-1)
+        return render_template('category.html',data=results,category=global_category, city=global_city) 
+    else:
+        return "잘못입력하셨습니다."
+
